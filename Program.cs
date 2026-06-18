f73055bd7472746faefc0e2673773b74c7b9d2e9
vika <vdanilova0987@gmail.com>
2026-06-18 19:31:32 +0300

    добавлен код

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.IO.Pipes;
+using System.Text;
+
+using var pipe = new NamedPipeServerStream("mypipe", PipeDirection.InOut);
+pipe.WaitForConnection();
+
+using var reader = new StreamReader(pipe, Encoding.UTF8);
+using var writer = new StreamWriter(pipe, Encoding.UTF8) { AutoFlush = true };
+
+string text = reader.ReadLine();
+File.AppendAllText("log.txt", $"{DateTime.Now}: {text}\n");
+writer.WriteLine(text.ToUpper());
